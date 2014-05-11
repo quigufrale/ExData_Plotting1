3f361ba874938ce24d0a29f211af90114a9bd607
Frank Q <quigufrale1184@yahoo.com>
2014-05-11 18:32:30 -0500

    Plot 1 - R Code

---
+++ plot1.R
@@ -0,0 +1,34 @@
+# Get Data
+# Set appropriate directory by using setwd
+# For Example:
+setwd("C:/Users/quigufrale/Documents/COURSERA/DataScienceSpecialization/SM14_4_ExploratoryDataAnalysis/w1/PrA")
+if(!file.exists("data")){dir.create("data")} # First create data folder
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, destfile="./data/household_power_consumption.zip") # Download file
+# I will use read.csv.sql from the sqldf package
+# for that I will need to unzip the file first
+unzip("./data/household_power_consumption.zip",
+      files = "household_power_consumption.txt", exdir = "./data")
+# Since the sqldf package is required, if not installed, 
+# uncomment the following line
+# install.packages("sqldf")
+library(sqldf)
+mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+PwrData <- read.csv.sql("./data/household_power_consumption.txt", sql=mySql, sep=";")
+# Checking missing data ?
+checkQM <- PwrData == "?"
+if (sum(colSums(checkQM)) == 0){print('No missing data')}
+
+# Create datetime column
+PwrData$Date<-as.Date(PwrData$Date, format = '%d/%m/%Y')
+PwrData$datetime <- paste(PwrData$Date, PwrData$Time)
+PwrData$datetime<-strptime(PwrData$datetime, "%Y-%m-%d %H:%M:%S")
+
+# plot 1
+# Histogram
+png(file = "./plot1.png", bg = "transparent", width = 480, height = 480, units = "px")
+hist(PwrData$Global_active_power, col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency")
+dev.off()
